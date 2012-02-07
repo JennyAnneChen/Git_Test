16f34fe45fcf86f2b6a62ac3a776ac02438f9e0f
Anne Chen <jenny.chen@microtek.com.tw>
2012-02-07 18:54:45 +0800

    Commit

---
+++ Test/Test/Protocol.h
@@ -0,0 +1,13 @@
+//
+//  Protocol.h
+//  Test
+//
+//  Created by jenny chen on 12/2/7.
+//  Copyright 2012年 microtek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol Protocol <NSObject>
+
+@end
