190c5f4de27351309c9f75e44ef07f3854350cea
Anne Chen <jenny.chen@microtek.com.tw>
2012-02-08 10:18:55 +0800

    Add Show

---
+++ Test/Test/showShow.h
@@ -0,0 +1,13 @@
+//
+//  showShow.h
+//  Test
+//
+//  Created by jenny chen on 12/2/8.
+//  Copyright 2012年 microtek. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface showShow : NSWindowController
+
+@end
