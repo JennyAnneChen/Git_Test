438cb645d766025df86f17203cb7e0bc59a90b32
Anne Chen <jenny.chen@microtek.com.tw>
2012-02-08 10:08:42 +0800

    Add File

---
+++ Test/Test/MyClass2.h
@@ -0,0 +1,13 @@
+//
+//  MyClass2.h
+//  Test
+//
+//  Created by jenny chen on 12/2/8.
+//  Copyright 2012年 microtek. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyClass2 : NSWindowController
+
+@end
