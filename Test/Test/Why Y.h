145688fa0487917d9c1c935c735fbfc2feb237f3
Anne Chen <jenny.chen@microtek.com.tw>
2012-02-08 10:21:43 +0800

    Y

---
+++ Test/Test/Why Y.h
@@ -0,0 +1,13 @@
+//
+//  Why Y.h
+//  Test
+//
+//  Created by jenny chen on 12/2/8.
+//  Copyright 2012年 microtek. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Why_Y : NSWindowController
+
+@end
---
+++ Test/Test/Why Y.m
@@ -0,0 +1,30 @@
+//
+//  Why Y.m
+//  Test
+//
+//  Created by jenny chen on 12/2/8.
+//  Copyright 2012年 microtek. All rights reserved.
+//
+
+#import "Why Y.h"
+
+@implementation Why_Y
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
