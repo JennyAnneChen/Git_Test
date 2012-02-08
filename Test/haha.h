dc6a1a7ab5eee08d2dabce42f05698c14e8478c3
Anne Chen <jenny.chen@microtek.com.tw>
2012-02-08 10:16:09 +0800

    Add Haha

---
+++ Test/haha.h
@@ -0,0 +1,13 @@
+//
+//  haha.h
+//  Test
+//
+//  Created by jenny chen on 12/2/8.
+//  Copyright 2012年 microtek. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface haha : NSWindowController
+
+@end
